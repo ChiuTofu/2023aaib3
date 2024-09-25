8757079c8e545bd2107d52588819990d3b76c06e
ChiuTofu <kevin.chiu0723@gmail.com>
2024-09-25 11:35:38 +0800

    week03

--- week03/week03-1.cpp
+++ week03/week03-1.cpp
@@ -1,3 +1,7 @@
+///week03-1.cpp考試題目:除惡務盡，但無法執行。
+///因為range-based for 迴圈必須在c++(2011之後)才能使用
+///如果使用c++98(1998年版本c++)會無法正確編譯，迴圈出不來
+///codeblacks 17.12裡勾選setting-compiler勾選第2個
 #include <iostream>
 #include <string>
 using namespace std;
---
+++ week03/week03-4.py
@@ -0,0 +1,5 @@
+#在草堆找到一根針
+#在python只要一行a.find(b)在字串a裡找到字串b
+class Solution:
+    def strStr(self, a: str, b: str) -> int:
+        return a.find(b)
