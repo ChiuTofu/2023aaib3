693e533013a5e5eb1ff563e307e19eda6040c962
ChiuTofu <kevin.chiu0723@gmail.com>
2024-11-20 09:55:10 +0800

    week11

---
+++ week11/week11-1.py
@@ -0,0 +1,11 @@
+# week11-1.py
+table = {}
+table[9797979] = 9
+table[4590685] = 4
+
+
+print(table[9797979])
+print(table[4590685])
+# print(table[0])
+if 0 not in table:
+  print("table[0]¤£¦s¦b")
